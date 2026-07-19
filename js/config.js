4a2c35b3f8e8b2eb78cc94fc5068747256729771
Sindy <jkentertainment2026@hotmail.com>
2026-07-20 00:26:08 +0700

    Add configuration constants for API keys and URLs

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const CONFIG = {
+    GEMINI_API_KEY: 'AQ.Ab8RN6K1uAX-76vd3LcU1Y33Y4tSXDgDodTV6G8ZjIo007zeww',
+    GAS_API_URL: 'https://script.google.com/macros/s/AKfycbzKWH_JBWHjmG3dRd3CAulDQ-Sn9FwV2GZYd-5lEQytjQnHJg_KcrTDrdRn2um6_QlL2w/exec'
+};
