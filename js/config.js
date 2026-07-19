3aaa928dd0c7fdaef87c75292e241040cd160c62
Sindy <jkentertainment2026@hotmail.com>
2026-07-20 00:57:20 +0700

    Update GEMINI_API_KEY in config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    GEMINI_API_KEY: 'AIzaSyBSslKYvXOyOc-tEKRptx451fTXe7OzVho',
+    GEMINI_API_KEY: 'AQ.Ab8RN6Ifo-6tnd5-Uk6navSz-Zlj681jBc1h0wAbwtdqBZe3YQ',
     GAS_API_URL: 'https://script.google.com/macros/s/AKfycbzKWH_JBWHjmG3dRd3CAulDQ-Sn9FwV2GZYd-5lEQytjQnHJg_KcrTDrdRn2um6_QlL2w/exec'
 };
